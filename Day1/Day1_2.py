12cd0515129991580c1ada193ea136a2958b3579
Shubham Muley <muleyshubham95@gmail.com>
2023-03-02 22:45:30 +0530

    Added python Day1 prog2

---
+++ Day1/Day1_2.py
@@ -0,0 +1,14 @@
+
+num=50 #single line statement
+print(num)
+
+#two statements on single line , saparate it with semicolon
+num1=25;num2=45
+print(num1)
+print(num2)
+
+#multiple lines are saparated by \
+
+result = 3+4+5+\
+         6+7
+print(result)
