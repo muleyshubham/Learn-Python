b73f92fa210802384342a517f22d526fe32333dd
Shubham Muley <muleyshubham95@gmail.com>
2023-03-02 22:51:12 +0530

    Changes in code day1 prog1

--- Day1/Day1_1.py
+++ Day1/Day1_1.py
@@ -1,7 +1,3 @@
-# First Python Program to Print Hello Everyone!!
-# Author : Akshita Chanchlani
-# Organization  :Sunbeam
-# Date : 13 th Feb 2023
 
 print("Hello Everyone!!")  #single line statement
 
