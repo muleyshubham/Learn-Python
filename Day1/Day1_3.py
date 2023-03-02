0123837170c3252b90966829c4803b145f9d1ffd
Shubham Muley <muleyshubham95@gmail.com>
2023-03-02 22:46:12 +0530

    Added Day1 prog3 python

---
+++ Day1/Day1_3.py
@@ -0,0 +1,18 @@
+# Demo on variables
+#mentioning of data types for variables is not compulsory
+# dynamically typed language
+
+num=50
+print(num)
+num=60
+print(num)
+
+#in python there is no concept of keyword const
+# but if we wish to specify any constant,
+# we follow convention
+# whenever we declare variable name in CAPITAL Letters
+# it is indication for the other developer that please consider it as constant
+
+PI=3.14
+print(PI)
+
